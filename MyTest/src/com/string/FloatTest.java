e60c7d1ed9a0f93ab778951c2e8a048ccafd2e08
王琪 <wangqi@huimin100.cn>
2018-03-29 17:07:35 +0800

    delete

--- MyTest/src/com/string/FloatTest.java
+++
@@ -1,8 +0,0 @@
-package com.string;
-
-public class FloatTest {
-	public static void main(String[] args) {
-		Float f = new Float(0.0005);
-		System.out.println(f);
-	}
-}
--- MyTest/src/com/tyiti/test/ThreadTest.java
+++
@@ -1,6 +0,0 @@
-package com.tyiti.test;
-
-public class ThreadTest {
-	public static void main(String[] args) {
-	}
-}
